10f839af55b7e17527599d8bc844b6e9d74a9c44
sobaau <15152226+sobaau@users.noreply.github.com>
2021-06-18 23:30:28 +1000

    GLM Added.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 #include "shader.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
